4742189234dd25326fd20f215428442f495f3a26
Abhishek <abhishek.sahu@navsoft.in>
2020-09-03 21:02:20 +0530

    Type fixed.

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class NetworkHelper {
   NetworkHelper({this.url});
   final String url;
-  future getData() async {
+  Future getData() async {
     http.Response response = await http.get(url);
     if (response.statusCode == 200) {
       String data = response.body;
