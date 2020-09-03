486abb0d2acde1d167ccc603d8246d443f3943e3
Abhishek <abhishek.sahu@navsoft.in>
2020-09-03 19:37:39 +0530

    API demo completed.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,15 +17,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+  }
+
+  void getData() async {
+    String url =
+        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02';
+    http.Response response = await http.get(url);
+    print(response.statusCode);
   }
 
   @override
   Widget build(BuildContext context) {
     String myMargin = '10';
     double myMarginAsDouble;
-
+    getData();
     try {
       myMarginAsDouble = double.parse(myMargin);
     } catch (e) {
